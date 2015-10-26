e794690311423352029564052ff5175ca7ceb4f7
Filip Ginter <ginter@cs.utu.fi>
2015-10-26 23:34:08 +0200

    autogenerated by https://github.com/spyysalo/jekyll-hook

--- gen_index/index_page.py
+++ gen_index/index_page.py
@@ -61,29 +61,35 @@ categories={(u"Documentation status",u"stub"):"""<span class="widespan" style="c
             (u"Documentation status",u"partial"):"""<span class="widespan"><span class="hint--top hint--info" data-hint="Partial documentation"><i class="fa fa-file-o"></i></span></span>""",
             (u"Documentation status",u"complete"):"""<span class="widespan"><span class="hint--top hint--info" data-hint="Complete documentation"><i class="fa fa-file-text-o"></i></span></span>""",
             (u"Data source",u"unknown"):"""<span class="widespan"><span class="hint--top hint--info" data-hint="Data source not known">-</span></span>""",
-            (u"Data source",u"automatic conversion"):"""<span class="widespan"><span class="hint--top hint--info" data-hint="Automatic conversion"><i class="fa fa-cogs"></i></span></span>""",
-            (u"Data source",u"automatic conversion + manual check"):"""<span class="widespan"><span class="hint--top hint--info" data-hint="Automatic conversion with manual corrections"><i class="fa fa-cogs"></i><!--<i class="fa fa-plus" style="font-size: 0.75em; line-height: 1.33em; vertical-align: +10%;">--><i class="fa fa-check"></i></span></span>""",
-            (u"Data source",u"native UD annotation"):"""<span class="widespan"><span class="hint--top hint--info" data-hint="Manual annotation"><i class="fa fa-user"></i></span></span>""",
+            (u"Data source",u"automatic"):"""<span class="widespan"><span class="hint--top hint--info" data-hint="Automatic conversion"><i class="fa fa-cogs"></i></span></span>""",
+            (u"Data source",u"semi-automatic"):"""<span class="widespan"><span class="hint--top hint--info" data-hint="Automatic conversion with manual corrections"><i class="fa fa-cogs"></i><!--<i class="fa fa-plus" style="font-size: 0.75em; line-height: 1.33em; vertical-align: +10%;">--><i class="fa fa-check"></i></span></span>""",
+            (u"Data source",u"manual"):"""<span class="widespan"><span class="hint--top hint--info" data-hint="Full manual check of the data"><i class="fa fa-user"></i></span></span>""",
             (u"License",u"none"):"""<span class="widespan"><span class="hint--top hint--info" data-hint="License not known">-</span></span>""",
             (u"Data available since",u"UD v1.0"):"""<span class="widespan"><span class="hint--top hint--info" data-hint="First released in UD version 1.0 (Jan 2015)"><i class="fa fa-check"></i></span></span>""",
             (u"Data available since",u"UD v1.1"):"""<span class="widespan"><span class="hint--top hint--info" data-hint="First released in UD version 1.1 (May 2015)"><i class="fa fa-check"></i></span></span>""",
             (u"Data available since",u"UD v1.2"):"""<span class="widespan"><span class="hint--top hint--info" data-hint="Will be released in UD version 1.2 (November 2015)"><i class="fa fa-hourglass-end"></i></span></span>""",
             (u"Data available since",u"none"):"""<span class="widespan"><span class="hint--top hint--info" data-hint="No firm schedule for data release">-</span></span>"""}
 
+empty_wide_span="""<span class="widespan"><span class="hint--top hint--info" data-hint="{hint:}">?</span></span>"""
 license_span="""<span class="widespan"><span class="hint--top hint--info" data-hint="{license:}">{licenseshort:}</span></span>"""
-for lic in ("CC BY-NC-SA 2.5","CC BY-NC-SA 3.0","CC BY-NC-SA 3.0 US","CC BY-NC-SA 4.0"):
-    categories[(u"License",lic)]=license_span.format(license=lic,licenseshort="""<img class="license"  src="logos/by-nc-sa.svg"/>""")
-for lic in ("GNU GPL Version 2","GPL","GPL 3.0"):
-    categories[(u"License",lic)]=license_span.format(license=lic,licenseshort="""<img class="license" src="logos/gpl.svg"/>""")
-for lic in ("CC BY-SA 4.0","CC BY-SA 3.0"):
-    categories[(u"License",lic)]=license_span.format(license=lic,licenseshort="""<img class="license" src="logos/by-sa.svg"/>""")
-for lic in ("CC BY 4.0",):
-    categories[(u"License",lic)]=license_span.format(license=lic,licenseshort="""<img class="license" src="logos/by.svg"/>""")
+def get_license_span(lic):
+    if "CC BY-NC-SA" in lic:
+        return license_span.format(license=lic,licenseshort="""<img class="license"  src="logos/by-nc-sa.svg"/>""")
+    elif "CC BY-SA" in lic:
+        return license_span.format(license=lic,licenseshort="""<img class="license" src="logos/by-sa.svg"/>""")
+    elif "CC BY" in lic:
+        return license_span.format(license=lic,licenseshort="""<img class="license" src="logos/by.svg"/>""")
+    elif "GPL" in lic:
+        return license_span.format(license=lic,licenseshort="""<img class="license" src="logos/gpl.svg"/>""")
+    else:
+        return license_span.format(license=lic,licenseshort=""" """)
 
-valueRe=re.compile(u"^([a-zA-Z ]+): ([A-Za-z0-9+. -]+)$")
+
+
+valueRe=re.compile(u"^([a-zA-Z ]+): (.+)$")
 #known_cats=set(cat for cat,val in categories)
 def analyze_readme(dir_name):
-    readme_data={u"Documentation status":u"stub",u"Data source":u"automatic conversion",u"License":u"none",u"Data available since":u"none", u"Genre":u"none"}
+    readme_data={u"Documentation status":u"stub",u"Data source":u"automatic conversion",u"License":u"none",u"Data available since":u"none", u"Genre":u"none",u"Contributors":u""}
     readmes=sorted(x for x in glob.glob(os.path.join(dir_name,"*")) if "readme" in x.lower())
     if not readmes: #No readme file!
         return readme_data
@@ -113,7 +119,7 @@ def get_flag_span(lang_name):
 
 lcodes=json.loads(open("lcodes.json").read())
 
-genres_map=json.loads(open("genre_emojis.json","r").read())
+genres_map=json.loads(open("genre_symbols.json","r").read())
 for k,v in genres_map.iteritems():
     genres_map[k]=v.replace("_","-")
 def get_genre_span(genres):
@@ -126,7 +132,10 @@ def get_genre_span(genres):
     return """<span class="doublewidespan"><span class="hint--top hint--info" data-hint="%s">%s</span></span>"""%(genres,spans)
             
 
+
 def gen_table(args):
+
+    jekyll_data=[] #this will go to jekyll then as data
     
     a_data=StringIO.StringIO()
     print >> a_data, "<!-- content of _includes/at_glance.html -->"
@@ -147,20 +156,41 @@ def gen_table(args):
         print >> a_data, get_column_icons(corpus_data)
         readme_data=analyze_readme(os.path.join(args.ud_data,"UD_"+l))
         print >> sys.stderr, l
-        for c in (u"Documentation status", u"Data source", u"Data available since", u"License"):
-            print >> a_data, categories[c,readme_data[c]]
+        for c in (u"Documentation status", u"Data source", u"Data available since"):
+            print >> a_data, categories.get((c,readme_data[c]),empty_wide_span.format(hint=readme_data[c]))
+        print >> a_data, get_license_span(readme_data[u"License"])
         print >> a_data, get_genre_span(readme_data["Genre"])
         print >> a_data, "</div>"
         print >> a_data, "<div>"
         print >> a_data, link_template.format(**corpus_data)
         print >> a_data, "</div>"
-    return a_data
+        
+        ldict={}
+        ldict[u"lang_name"]=corpus_data[u"lang_name"]
+        ldict[u"lang_code"]=corpus_data[u"lang_code"]
+        ldict[u"contributors"]=[]
+        if readme_data["Contributors"].strip():
+            for c in readme_data["Contributors"].strip().split(u";"):
+                c=c.strip()
+                lf=c.split(u",",1)
+                if len(lf)==2:
+                    ldict[u"contributors"].append({u"last":lf[0].strip(),u"first":lf[1].strip(), u"full":lf[1].strip()+u" "+lf[0].strip()})
+                else:
+                    ldict[u"contributors"].append({u"last":c,u"first":u"?",u"full":c})
+        jekyll_data.append(ldict)
+    return a_data,jekyll_data
 
 if __name__=="__main__":
     import argparse
     parser = argparse.ArgumentParser(description='generates the index page')
     parser.add_argument('--ud-data', required=True, help='Where is the UD data, so I can grab the readmes? (DIRECTORY)')
+    parser.add_argument('--ldict', default="../_data/ldata.json", help='Where to write the language dict file? (Default %(default)s)')
     args = parser.parse_args()
     
-    a_data=gen_table(args)
+    a_data,ldict=gen_table(args)
     print a_data.getvalue()
+    if args.ldict:
+        with open(args.ldict,"w") as out:
+            json.dump(ldict,out,indent=2)
+
+    
